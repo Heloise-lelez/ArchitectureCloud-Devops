51a69a396dff7999a4a26000f0cd77e51adca747
Héloïse Le Lez <lelez.heloise@gmail.com>
2025-07-22 20:53:14 +0200

    🎉 first commit

---
+++ TP2/tp2/infra/variable.tf
@@ -0,0 +1,44 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket to create"
+  type        = string
+  default     = "iim-devops-tp2-fepvnegvivzpvhzprbrmqbry-26375"
+
+}
+
+variable "tags" {
+  type = object({
+    Name        = string
+    Environment = string
+  })
+  default = {
+    Name        = "tp_bucket"
+    Environment = "dev"
+  }
+}
+
+
+variable "mime_types" {
+  description = "Mapping of file extensions to their respective MIME (Multipurpose Internet Mail Extensions) types. This helps in determining the nature and format of a document."
+  type        = map(string)
+  default = {
+    htm  = "text/html"
+    html = "text/html"
+    css  = "text/css"
+    ttf  = "font/ttf"
+    js   = "application/javascript"
+    map  = "application/javascript"
+    json = "application/json"
+  }
+}
+
+variable "sync_directories" {
+  type = list(object({
+    local_source_directory = string
+    s3_target_directory    = string
+  }))
+  description = "List of directories to synchronize with Amazon S3."
+  default = [{
+    local_source_directory = "../dist"
+    s3_target_directory    = ""
+  }]
+}
